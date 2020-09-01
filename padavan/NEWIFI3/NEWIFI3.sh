2d8ec89db30c8b1302e85f8373ad4e6fe23275bf
wuping24 <wuping24@gmail.com>
2020-09-01 11:32:24 +0800

    Update NEWIFI3.sh

--- padavan/NEWIFI3/NEWIFI3.sh
+++ padavan/NEWIFI3/NEWIFI3.sh
@@ -21,12 +21,12 @@ echo "CONFIG_FIRMWARE_INCLUDE_CADDYBIN=n" >> .config     # 集成 caddu执行文
 
 # 广告
 echo "CONFIG_FIRMWARE_INCLUDE_KOOLPROXY=n" >> .config    # KP 广告过滤
-echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOME=y" >> .config  # ADGUARD 广告拦截
+echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOME=n" >> .config  # ADGUARD 广告拦截
 
 # 代理
 echo "CONFIG_FIRMWARE_INCLUDE_KUMASOCKS=y" >> .config    # KUMA
 
 # 穿透
-echo "CONFIG_FIRMWARE_INCLUDE_FRPC=y" >> .config         # 内网穿透FRPC
-echo "CONFIG_FIRMWARE_INCLUDE_FRPS=y" >> .config         # 内网穿透FRPS
+echo "CONFIG_FIRMWARE_INCLUDE_FRPC=n" >> .config         # 内网穿透FRPC
+echo "CONFIG_FIRMWARE_INCLUDE_FRPS=n" >> .config         # 内网穿透FRPS
 echo "CONFIG_FIRMWARE_INCLUDE_ALIDDNS=y" >> .config      # 阿里DDNS
